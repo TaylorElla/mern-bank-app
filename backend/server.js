382a85422e5d81f6a45cd3565462596e42dfca05
Ella Taylor <110649790+TaylorElla@users.noreply.github.com>
2023-05-17 19:59:41 -0700

    testing without path

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require('express')
-const path = require('path')
+
 const cors = require('cors') 
 const mongoose = require('mongoose')
 const transactionRoutes = require('./routes/transactions')

861e5bfa82a5909329e56d99795441a533aa13d2
Ella Taylor <110649790+TaylorElla@users.noreply.github.com>
2023-05-20 16:04:07 -0700

    update navbar styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,10 +2,8 @@
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
 import { useAuthContext } from '../hooks/useAuthContext';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+
 
 const CustomNavbar = () => {
   const { logout } = useLogout();
@@ -17,8 +15,8 @@ const CustomNavbar = () => {
   };
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
+    <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg" style={{ boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)' }}>
+      <Container fluid>
         <Navbar.Brand as={Link} to="/">
           <div className="brand-content">
             <img
@@ -32,36 +30,46 @@ const CustomNavbar = () => {
           </div>
         </Navbar.Brand>
 
-        {user && (
-          <Nav className="mr-auto">
-            <Nav.Item>
-              <Nav.Link as={Link} to="/MyAccount">
-                MyAccount
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        )}
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          {user && (
+            <Nav className="mr-auto">
+              <Nav.Item>
+                <Nav.Link as={Link} to="/MyAccount">
+                  MyAccount
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          )}
+
           <Nav className="ml-auto">
-            {user && (
-              <Nav.Item className="nav-link">
-                <span>{user.email}</span>
+          {user && (
+            <>
+              <Nav.Item>
+                <Nav.Link><span>{user.email}</span></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
                 <Button variant="outline-primary" onClick={handleClick}>
                   Log out
                 </Button>
               </Nav.Item>
-            )}
+            </>
+          )}
             {!user && (
-              <Nav.Item className="authLinks">
-                <Nav.Link as={Link} to="/login">
-                  Login
-                </Nav.Link>
-                <Nav.Link as={Link} to="/signup">
-                  Signup
-                </Nav.Link>
-              </Nav.Item>
+              <>
+                <Nav.Item className="authLinks">
+                  <Nav.Link as={Link} to="/login">
+                    Login
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item className="authLinks">
+                  <Nav.Link as={Link} to="/signup">
+                    Signup
+                  </Nav.Link>
+                </Nav.Item>
+              </>
             )}
           </Nav>
         </Navbar.Collapse>
